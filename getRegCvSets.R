5eaed50d431245df7bc6a0f9c041f74deb5e04e4
FlexDW <felixward@gmail.com>
2016-10-21 17:03:16 +0200

    Update getRegCvSets.R

--- getRegCvSets.R
+++ getRegCvSets.R
@@ -1,26 +1,14 @@
-getRegCvSets <- function(y, nsets=5, seed=NULL, print=TRUE){
+getRegCvSets <- function(y, nsets=5, seed=NULL){
 # Returns cross validation set foldid.
 
   if(!is.null(seed)){
     set.seed(seed)
   }
   
-  # created vector to order case and controls
-  index <- 1:length(y)
-  index <- index[order(y)]
-  
   # generate foldids
   cvSet <- rep(1:nsets, length=length(y))
+  cvSet <- sample(cvSet)
 
-  # reorder according to original vector
-  cvSet <- cvSet[order(index)]
-  
-  # print allocation per fold
-  if(print){
-    print(tapply(y, INDEX=cvSet, median))
-    print(tapply(y, INDEX=cvSet, mean))
-  }
-  
   return(cvSet)
 }
 
